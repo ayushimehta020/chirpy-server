19f54f0e8e888b3bf668a62ead1e73a22b0822f3
ayushimehta020 <ayushimehta020@gmail.com>
2026-06-30 15:37:05 +0530

    sort chirps ascending or descending by date of creation

--- src/handlers/handler_get_all_chirps.ts
+++ src/handlers/handler_get_all_chirps.ts
@@ -2,11 +2,19 @@ import { Request, Response } from "express";
 import { getAllChirps } from "../db/queries/chirps.js";
 
 export async function handlerGetAllChirps(req: Request, res: Response) {
-  const { authorId } = req.query;
+  const { authorId, sort } = req.query;
 
   const chirps = await getAllChirps(
     typeof authorId === "string" ? authorId : undefined,
   );
+
+  chirps.sort((a, b) => {
+    if (sort === "desc") {
+      return b.createdAt.getTime() - a.createdAt.getTime();
+    }
+
+    return a.createdAt.getTime() - b.createdAt.getTime();
+  });
 
   res.json(chirps);
 }
